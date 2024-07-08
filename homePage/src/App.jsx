158b435d9ae0beaaa5eddc64d8bff8996a78991c
agent-52 <abhaybhadauriya52@gmail.com>
2024-07-08 16:08:02 +0530

    makes page1 without the 3d planet

---
+++ homePage/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react"
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+import * as CANNON from 'cannon-es'
+import Header from "./Header/Header"
+import "./App.css"
+import earthi from "./assets/earthi.png"
+import simVideo from "./assets/simVideo.mp4"
+
+
+const App = () =>
+{
+  useEffect(()=>{
+
+
+  })
+
+  return(
+    <div className="body background pi1 text-slate-12 font-sans">
+
+      <Header/>
+      <canvas className="webgl"></canvas>
+      <div className="flexC gap2 align-center section1">
+        <div className="p1h1 flexC align-center">
+          <h1 className="text-large noWrap">W<div className="o"><video src={simVideo} autoPlay loop muted></video></div>rld's best</h1>
+          <h1 className="text-large">marketplace</h1>
+        </div>
+        <div className="p1h2 glassyText text-large font-medium flexC align-center f6">
+          <div>for</div>
+          <div>Virtual Estates</div>
+        </div>
+        
+        <div className="enterWorld flex gap0 align-center">
+          <div className="imgBox"><img src={earthi} alt="" /></div>
+          <div>Dive Into Future</div>
+        </div>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ homePage/src/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css"
+import shi1 from "../assets/insta.png"
+import shi2 from "../assets/twitter.png"
+import shi3 from "../assets/yt.png"
+const Header = () =>{
+
+  return(
+    <div className="header text-slate-12 text-sm flex justify-between gap1 align-center">
+      <div className="logo font-medium">WeLand</div>
+      <div className="hNav text-slate-11 flex gap2">
+        <div>Vision</div>
+        <div>Features</div>
+        <div>Team</div>
+        <div>Updates</div>
+      </div>
+      <div className="flex gap1">
+        <div className="hImgBar flex gap1 align-center">
+          <div className="imgBox"><img src={shi1} alt="" /></div>
+          <div className="imgBox"><img src={shi2} alt="" /></div>
+          <div className="imgBox"><img src={shi3} alt="" /></div>
+        </div>
+        <div className="explore text-sm1">Register</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
