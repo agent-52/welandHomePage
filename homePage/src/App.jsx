c9a1de7391d693be297d24062ffccdd0c0dc7607
agent-52 <abhaybhadauriya52@gmail.com>
2024-07-12 12:33:22 +0530

    complete page1 exept canvas

--- homePage/src/App.jsx
+++ homePage/src/App.jsx
@@ -7,12 +7,13 @@ import Header from "./Header/Header"
 import "./App.css"
 import earthi from "./assets/earthi.png"
 import simVideo from "./assets/simVideo.mp4"
+import gsap from "gsap"
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 
 const App = () =>
 {
   useEffect(()=>{
-
 
   })
 
@@ -34,6 +35,12 @@ const App = () =>
         <div className="enterWorld flex gap0 align-center">
           <div className="imgBox"><img src={earthi} alt="" /></div>
           <div>Dive Into Future</div>
+        </div>
+      </div>
+      <div className="section2">
+        <div className="flexC gap1 text-center">
+          <div className="text-medium ">Buy Sell Explore</div>
+          <div className="text-sm1 text-slate-11 description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita, perspiciatis. Corrupti, vitae ratione asperiores nihil iste consectetur, nulla necessitatibus quos doloribus a culpa placeat nisi, incidunt at doloremque sapiente sequi?</div>
         </div>
       </div>
       
